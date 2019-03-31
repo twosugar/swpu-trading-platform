71e68c1ec36bf2b66f0f56a9fc3a48ebeb54e1d6
twosugar <sugarfish@yeah.net>
2019-04-01 00:48:31 +0800

    评论区

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,8 +5,11 @@ Page({
    * 页面的初始数据
    */
   data: {
-    proDetail: "",
-    commentData: []
+    // proDetail: "",
+    proDetail: JSON.parse(`{"_id":"XJEFqZT75u22qvAD","_openid":"o9Q2L5bifFYK9VLGpay - AOwFjy - g","content":"无价风景","imgList":["https://7879-xyz-63b968-1258737955.tcb.qcloud.la/userImg/1553008037132-5087450.jpg"],"mobile":{"require":true,"value":"16772993"},"price":6,"send_time":1553008040385,"title":"学校风景","type":"中国","user_img":"https://wx.qlogo.cn/mmopen/vi_32/Cjf0tRZajBV857YglONyicgCdd2FqqzkRic7cLdMae0siaKES5yCw9nOv8geELD2GlLxowyAOGUt6iaRAHfjblDnRg/132","username":"乞人","wx":{"require":true,"value":"xf9955"}}`),
+    commentData: [],
+    userInfo: '',
+    isClickBtn: false,//是否点击留言
   },
 
   /**
@@ -14,19 +17,95 @@ Page({
    */
   onLoad: function (options) {
     console.log(options)
+    this.isUserLogin()
+    // if (!options.data) {
+    //   return
+    // }
     this.setData({
-      proDetail: JSON.parse(options.data)
+      // proDetail: JSON.parse(options.data),
     }, () => {
-      this.onGetCommentList(this.data.proDetail._id)
+      this.onGetCommentList(this.data.proDetail._id || 'XJEFqZT75u22qvAD')
     })
     
+  },
+
+  isUserLogin() {
+    const that = this
+    wx.getSetting({
+      success: function (res) {
+        console.log('res', res)
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称
+          wx.getUserInfo({
+            success: function (res) {
+              console.log('res', res.userInfo, res)
+              that.setData({
+                userInfo: res.userInfo
+              })
+            }
+          })
+        }
+        else {
+          wx.navigateTo({
+            url: '../../pages/firstPage/firstPage',
+          })
+        }
+      }
+    })
+  },
+
+  addComment() {//点击留言
+    this.setData({
+      isClickBtn: true
+    })
+  },
+
+  closeAddComment() {//失去焦点
+    this.setData({
+      isClickBtn: false
+    })
+  },
+
+  inputChange(e) {
+    this.setData({
+      inputValue: e.detail.value
+    })
+  },
+
+  sendComment() {
+    console.log(213)
+    const { inputValue, userInfo } = this.data
+    const that = this
+    const db = wx.cloud.database()
+    const _ = db.command
+    const id = this.data.id
+    const nowTime = new Date().getTime()
+    const params = {
+      commentContent: inputValue || '',
+      commentId: parseInt(Math.random() * 10000000000) || '',
+      commentTime: nowTime || '',
+      identity: '楼主',
+      userName: userInfo.nickName || '',
+      img: userInfo.avatarUrl || ''
+    }
+    console.log('params', params, id)
+    db.collection('comment').doc(id).update({
+      data: {
+        0: _.push(params)
+      },
+      success(res) {
+        console.log(res)
+      },
+      fail(res) {
+        console.log(res)
+      }
+    })
   },
 
   onGetCommentList: function (id) {
     console.log('id', id)
     const that = this
     const db = wx.cloud.database()
-    // 查询当前用户所有的 counters
     db.collection('comment').where({
       "proId": id
     }).get({
@@ -36,7 +115,8 @@ Page({
           return
         }
         this.setData({
-          commentData: res.data
+          commentData: res.data,
+          id: res.data[0]._id
         })
         wx.hideNavigationBarLoading()
         wx.stopPullDownRefresh()
